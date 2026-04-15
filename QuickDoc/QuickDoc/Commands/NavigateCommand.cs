f7b35e4d70ec5bbfec49ac6adabc10a8d8361de5
Martin Henriksen <martin.henriksen@3mj.dk>
2026-04-15 12:18:30 +0200

    on god

--- QuickDoc/QuickDoc/Commands/NavigateCommand.cs
+++ QuickDoc/QuickDoc/Commands/NavigateCommand.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using QuickDoc.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace QuickDoc.Commands
 {
-    public class NavigateCommand
+    public class NavigateCommand : ICommand
     {
+        public event EventHandler? CanExecuteChanged;
 
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
